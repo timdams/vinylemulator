a18a04f38bbfc7502bafe0258a94dd62ee340780
Tim Dams <dams.tim@telenet.be>
2025-06-12 08:09:37 +0200

    Update readnfc_myversion.py

--- readnfc_myversion.py
+++ readnfc_myversion.py
@@ -1,3 +1,4 @@
+
 import time
 import nfc
 import requests
@@ -79,9 +80,9 @@ def touched(tag):
 
             #if no service or command detected, exit
             if servicetype == "":
-                print ("Service type not recognised. NFC tag text should begin s                                                                                                                                                                             potify, tunein, amazonmusic, apple/applemusic, command or room.")
+                print ("Service type not recognised. NFC tag text should begin spotify, tunein, amazonmusic, apple/applemusic, command or room.")
                 if usersettings.sendanonymoususagestatistics == "yes":
-                    r = requests.post(appsettings.usagestatsurl, data = {'time':                                                                                                                                                                              time.time(), 'value1': appsettings.appversion, 'value2': hex(uuid.getnode()), '                                                                                                                                                                             value3': 'invalid service type sent'})
+                    r = requests.post(appsettings.usagestatsurl, data = {'time': time.time(), 'value1': appsettings.appversion, 'value2': hex(uuid.getnode()), 'value3': 'invalid service type sent'})
                 return True
 
             print ("Detected " + servicetype + " service request")
@@ -90,30 +91,40 @@ def touched(tag):
             if servicetype.lower() == 'completeurl':
                 urltoget = sonosinstruction
             else:
-                urltoget = usersettings.sonoshttpaddress + "/" + sonosroom_local                                                                                                                                                                              + "/" + sonosinstruction
+                urltoget = usersettings.sonoshttpaddress + "/" + sonosroom_local + "/" + sonosinstruction
 
             #check Sonos API is responding
             try:
                 r = requests.get(usersettings.sonoshttpaddress)
             except:
-                print ("Failed to connect to Sonos API at " + usersettings.sonos                                                                                                                                                                             httpaddress)
+                print ("Failed to connect to Sonos API at " + usersettings.sonoshttpaddress)
                 return True
 
             #clear the queue for every service request type except commands
             if servicetype != "command":
                 print ("Clearing Sonos queue")
-                r = requests.get(usersettings.sonoshttpaddress + "/" + sonosroom                                                                                                                                                                             _local + "/clearqueue")
+                r = requests.get(usersettings.sonoshttpaddress + "/" + sonosroom_local + "/clearqueue")
+
+            # volume instellen
+            time.sleep(0.1)
+            requests.get(usersettings.sonoshttpaddress + "/" + sonosroom_local + "/volume/8")
+            time.sleep(0.1)
+            requests.get(usersettings.sonoshttpaddress + "/tvkamer/volume/8")
+
+
 
             #  shuffle gedrag instellen
+            time.sleep(0.2)
             if shuffle:
                    print("Enabling shuffle for  playlist")
-                   r = requests.get(usersettings.sonoshttpaddress + "/" + sonosr                                                                                                                                                                             oom_local + "/shuffle/on")
+                   r = requests.get(usersettings.sonoshttpaddress + "/" + sonosroom_local + "/shuffle/on")
             else:
                    print("Disabling shuffle for  (not a playlist)")
-                   r = requests.get(usersettings.sonoshttpaddress + "/" + sonosr                                                                                                                                                                             oom_local + "/shuffle/off")
+                   r = requests.get(usersettings.sonoshttpaddress + "/" + sonosroom_local + "/shuffle/off")
 
 
-            #use the request function to get the URL built previously, triggerin                                                                                                                                                                             g the sonos
+            time.sleep(0.2)
+            #use the request function to get the URL built previously, triggering the sonos
             print ("Fetching URL via HTTP: "+ urltoget)
             r = requests.get(urltoget)
 
@@ -125,7 +136,8 @@ def touched(tag):
 
 
             # --- nieuw blok: living joinen indien
-            requests.get(usersettings.sonoshttpaddress + "/tvkamer/join/" + sono                                                                                                                                                                             sroom_local)
+            time.sleep(0.2)
+            requests.get(usersettings.sonoshttpaddress + "/tvkamer/join/" + sonosroom_local)
             # --- einde nieuw blok ---
 
 
@@ -144,9 +156,9 @@ def touched(tag):
 
     else:
         print("")
-        print ("NFC reader could not read tag. This can be because the reader di                                                                                                                                                                             dn't get a clear read of the card. If the issue persists then this is usually be                                                                                                                                                                             cause (a) the tag is encoded (b) you are trying to use a mifare classic card, wh                                                                                                                                                                             ich is not supported or (c) you have tried to add data to the card which is not                                                                                                                                                                              in text format. Please check the data on the card using NFC Tools on Windows or                                                                                                                                                                              Mac.")
+        print ("NFC reader could not read tag. This can be because the reader didn't get a clear read of the card. If the issue persists then this is usually because (a) the tag is encoded (b) you are trying to use a mifare classic card, which is not supported or (c) you have tried to add data to the card which is not in text format. Please check the data on the card using NFC Tools on Windows or Mac.")
         if usersettings.sendanonymoususagestatistics == "yes":
-            r = requests.post(appsettings.usagestatsurl, data = {'time': time.ti                                                                                                                                                                             me(), 'value1': appsettings.appversion, 'value2': hex(uuid.getnode()), 'value3':                                                                                                                                                                              'nfcreaderror'})
+            r = requests.post(appsettings.usagestatsurl, data = {'time': time.time(), 'value1': appsettings.appversion, 'value2': hex(uuid.getnode()), 'value3': 'nfcreaderror'})
 
     return True
 
@@ -166,14 +178,14 @@ try:
 except IOError as e:
     print ("... could not connect to reader")
     print ("")
-    print ("You should check that the reader is working by running the following                                                                                                                                                                              command at the command line:")
+    print ("You should check that the reader is working by running the following command at the command line:")
     print (">  python -m nfcpy")
     print ("")
-    print ("If this reports that the reader is in use by readnfc or otherwise cr                                                                                                                                                                             ashes out then make sure that you don't already have readnfc running in the back                                                                                                                                                                             ground via pm2. You can do this by running:")
-    print (">  pm2 status             (this will show you whether it is running)                                                                                                                                                                             ")
-    print (">  pm2 stop readnfc       (this will allow you to stop it so you can                                                                                                                                                                              run the script manually)")
+    print ("If this reports that the reader is in use by readnfc or otherwise crashes out then make sure that you don't already have readnfc running in the background via pm2. You can do this by running:")
+    print (">  pm2 status             (this will show you whether it is running)")
+    print (">  pm2 stop readnfc       (this will allow you to stop it so you can run the script manually)")
     print ("")
-    print ("If you want to remove readnfc from running at startup then you can d                                                                                                                                                                             o it with:")
+    print ("If you want to remove readnfc from running at startup then you can do it with:")
     print (">  pm2 delete readnfc")
     print (">  pm2 save")
     print (">  sudo reboot")
@@ -192,7 +204,7 @@ print ("Trying to connect to API ...")
 try:
     r = requests.get(usersettings.sonoshttpaddress)
 except:
-    print ("... but API did not respond. This could be a temporary error so I wo                                                                                                                                                                             n't quit, but carry on to see if it fixes itself")
+    print ("... but API did not respond. This could be a temporary error so I won't quit, but carry on to see if it fixes itself")
 
 if r.status_code == 200:
     print ("... and API responding")
@@ -202,7 +214,7 @@ print("OK, all ready! Present an NFC tag.")
 print("")
 
 if usersettings.sendanonymoususagestatistics == "yes":
-    r = requests.post(appsettings.usagestatsurl, data = {'time': time.time(), 'v                                                                                                                                                                             alue1': appsettings.appversion, 'value2': hex(uuid.getnode()), 'value3': 'appsta                                                                                                                                                                             rt'})
+    r = requests.post(appsettings.usagestatsurl, data = {'time': time.time(), 'value1': appsettings.appversion, 'value2': hex(uuid.getnode()), 'value3': 'appstart'})
 
 while True:
     reader.connect(rdwr={'on-connect': touched, 'beep-on-connect': False})
